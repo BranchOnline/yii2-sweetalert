aa73992b884078ac7ce4676cdb046492f99191bd
Jap Mul Eindbaas <jap@branchonline.nl>
2015-11-20 08:47:43 +0100

    Updated the paths

--- src/SweetAlertAsset.php
+++ src/SweetAlertAsset.php
@@ -14,14 +14,14 @@ class SweetAlertAsset extends AssetBundle {
      * @var string the directory that contains the source asset files for this asset bundle.
      * A source asset file is a file that is part of your source code repository of your Web application.
      */
-    public $sourcePath = '@bower/sweetalert/lib';
+    public $sourcePath = '@bower/sweetalert/dist';
 
     /**
      * @var array list of JavaScript files that this bundle contains. Each JavaScript file can be
      * specified in one of the following formats:
      */
     public $js = [
-        'sweet-alert.min.js',
+        'sweetalert.min.js',
     ];
 
     /**
@@ -29,7 +29,7 @@ class SweetAlertAsset extends AssetBundle {
      * in one of the three formats as explained in [[js]].
      */
     public $css = [
-        'sweet-alert.css'
+        'sweetalert.css'
     ];
 
 }
